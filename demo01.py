e9875a003ce2f4934f6b5a30b204dcd1e1bf0d00
asheng <1029479451@qq.com>
2020-10-12 20:40:57 +0800

    第一次向guthub提交代码

---
+++ demo01.py
@@ -0,0 +1,27 @@
+"""print("你好") #字符串
+print(2333) #整数
+print(2.333) #小数
+print(True) #布尔值
+print(()) #元组
+print({}) #字典
+print([]) #数组
+
+print(23,0.11,"你好")
+print("哈哈"+"嘻嘻")
+print("哈哈"*33)
+print((3*33-9)/2)
+print(2<3)
+
+
+name = "张三" #把张三这个值赋值给了name这个变量
+"""
+
+print(type("哈哈"))
+print(type(233))
+print(type(2.33))
+print(type(True))
+print(type(()))
+print(type({}))
+print(type([]))
+
+
